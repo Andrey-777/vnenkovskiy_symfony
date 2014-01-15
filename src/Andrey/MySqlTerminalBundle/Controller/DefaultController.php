56d0b8998fcf96140d792eafa6c4e1f76188c019
avnenkovskyi <avnenkovskyi@avnenkovskyi-H55M-S2.(none)>
2014-01-15 12:39:04 +0200

    create populatQueriesHistory method in MysqlterminalServic

--- src/Andrey/MySqlTerminalBundle/Controller/DefaultController.php
+++ src/Andrey/MySqlTerminalBundle/Controller/DefaultController.php
@@ -3,43 +3,19 @@
 namespace Andrey\MySqlTerminalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Andrey\MySqlTerminalBundle\Entity\Product;
-//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
-use Andrey\MySqlTerminalBundle\Services\MysqlterminalService;
 use Andrey\MySqlTerminalBundle\Form\MysqlterminalForm;
 class DefaultController extends Controller
 {
-    protected $service    = null;
-    protected $showResult = false;
-
-    public function __construct()
-    {
-        $this->service = new MysqlterminalService();
-    }
-
     public function indexAction(Request $request)
     {
+        $session = $this->get('request')->getSession();
+        $service = $this->get('service.service');
+
         return $this->render('AndreyMySqlTerminalBundle:Default:index.html.twig',
-                             $this->service->checkRequest($request,
-                                                          $this->createForm(new MysqlterminalForm),
-                                                          $this->getDoctrine()->getManager()));
+            $service->checkRequest($request,
+                $this->createForm(new MysqlterminalForm),
+                $this->getDoctrine()->getManager(),
+                $session));
     }
-
-
-//    public function createAction()
-//    {
-//        $product = new Product();
-//        $product->setName('A Foo Bar');
-//        $product->setPrice('19.99');
-//        $product->setDescription('Lorem ipsum dolor');
-//        $product->setBlabla('AAAAAAAAAAAA');
-//
-//        $em = $this->getDoctrine()->getEntityManager();
-//        $em->persist($product);
-//        $em->flush();
-//
-//        return new Response('Created product id '.$product->getId());
-//    }
 }
--- src/Andrey/MySqlTerminalBundle/Services/MysqlterminalService.php
+++ src/Andrey/MySqlTerminalBundle/Services/MysqlterminalService.php
@@ -3,44 +3,61 @@ namespace Andrey\MySqlTerminalBundle\Services;
 use Symfony\Component\HttpFoundation\Request;
 
 use Andrey\MySqlTerminalBundle\Models\MysqlterminalModel;
+use Symfony\Component\HttpFoundation\Session\Session;
 class MysqlterminalService {
     protected $model      = null;
     protected $showResult = false;
+    protected $queriesHistory = false;
 
     public function __construct()
     {
         $this->model = new MysqlterminalModel();
     }
 
-    public function checkRequest(Request $request, $form, $em = null)
+    public function checkRequest(Request $request, $form, $em = null, $session = null)
     {
+
         if ($this->isPOST($request)) {
             $form->bind($request);
 
             if ($form->isValid()) {
-                $arr        = $form->getData();
+                $postData   = $form->getData();
                 $connection = $this->model->getConnectDB($em);
-                $statement  = $this->model->getStatementDB($connection, $arr['Query:']);
+                $statement  = $this->model->getStatementDB($connection, $postData['Query:']);
                 $results    = $this->model->getResult($statement);
 
+                if (!$this->model->errorMessage) {
+                    $session->set('queriesHistory',
+                        $this->populatQueriesHistory((array)$session->get('queriesHistory'),
+                            $postData['Query:']));
+                }
+
                 return array(
-                                'form'         => $form->createView(),
-                                'showResult'   => $this->showResult = true,
-                                'results'      => $results,
-                                'errorMessage' => $this->model->errorMessage
-                            );
+                    'form'           => $form->createView(),
+                    'showResult'     => $this->showResult = true,
+                    'results'        => $results,
+                    'errorMessage'   => $this->model->errorMessage,
+                    'queriesHistory' => $session->get('queriesHistory')
+                );
             }
         }
 
         return array(
-                        'form'         => $form->createView(),
-                        'showResult'   => $this->showResult,
-                        'errorMessage' => $this->model->errorMessage
-                    );
+            'form'         => $form->createView(),
+            'showResult'   => $this->showResult,
+            'errorMessage' => $this->model->errorMessage,
+            'queriesHistory' => $this->queriesHistory
+        );
     }
 
-    public function isPOST($request)
+    protected function isPOST($request)
     {
         return $request->getMethod() == 'POST' ? : false;
+    }
+
+    protected function populatQueriesHistory(Array $queries, $query)
+    {
+        array_unshift($queries, $query);
+        return array_unique($queries);
     }
 } 
