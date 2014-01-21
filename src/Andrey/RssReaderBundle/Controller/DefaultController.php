08621b6e2d4d784a7d46c024719835ec456c7d30
avnenkovskyi <avnenkovskyi@avnenkovskyi-H55M-S2.(none)>
2014-01-21 19:01:36 +0200

    add updateAction DefaultController.php, create updateResponse.html.twig, add _getContentFile, _getContentRss, _populateNewsAndChanel methods to RssReaderService.php

--- src/Andrey/RssReaderBundle/Controller/DefaultController.php
+++ src/Andrey/RssReaderBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace Andrey\RssReaderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use \Exception;
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+
+
         return $this->render('AndreyRssReaderBundle:Default:index.html.twig');
+    }
+
+    public function updateAction()
+    {
+        try {
+            $service = $this->get('RssReaderService.service');
+
+            return $this->render(
+                'AndreyRssReaderBundle:Default:updateResponse.html.twig',
+                    $service->updateMethod($this->get('kernel'))
+            );
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
--- src/Andrey/RssReaderBundle/Services/RssReaderService.php
+++ src/Andrey/RssReaderBundle/Services/RssReaderService.php
@@ -8,7 +8,65 @@
 
 namespace Andrey\RssReaderBundle\Services;
 
-
+use Symfony\Component\DependencyInjection\SimpleXMLElement;
+use \Exception;
 class RssReaderService {
+    protected $_listChannels = array();
+    protected $_listNews     = array();
 
+    public function updateMethod($kernel)
+    {
+        $linksToRss = $this->_getContentFile($kernel);
+        $contentRss = $this->_getContentRss($linksToRss);
+        $this->_populateNewsAndChanel($contentRss);
+
+        return array();
+    }
+
+    protected function _getContentFile($kernel)
+    {
+        try {
+            $content    = file_get_contents($kernel->locateResource('@AndreyRssReaderBundle/Files/links.txt'));
+            $linksToRss = array_filter(explode("\r\n", $content));
+
+            return $linksToRss;
+        } catch (Exception $e) {
+            $this->errorMessage =  $e->getMessage();
+        }
+    }
+
+    protected function _getContentRss($linksToRss)
+    {
+        foreach ($linksToRss as $itemLinkRss) {
+            $contentRss[] = file_get_contents($itemLinkRss);
+        }
+
+        return $contentRss;
+    }
+
+    protected function _populateNewsAndChanel($contentsRss)
+    {
+        foreach ($contentsRss as $keyXMLFile => $itemXMLFile) {
+            $sxml = new SimpleXMLElement($itemXMLFile, LIBXML_NOCDATA);
+            $this->_listChannels[$keyXMLFile]['title'] = (string)$sxml->channel->title;
+            $this->_listChannels[$keyXMLFile]['link']  = (string)$sxml->channel->link;
+
+            foreach ($sxml->channel->item as $keyNews => $itemNews) {
+                $itemNewsForArr['title']       = $itemNews->title;
+                $itemNewsForArr['link']        = $itemNews->link;
+                $itemNewsForArr['description'] = $itemNews->description;
+
+                if ((string)$sxml->channel->link == 'http://tsn.ua/') {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i',
+                        (string)$itemNews->description, $matches);
+                    $itemNewsForArr['enclosure'] = $matches[1][0];
+                } else {
+                    $itemNewsForArr['enclosure']   = property_exists($itemNews, 'enclosure') ? $itemNews->enclosure['url'] : '';
+                }
+                $itemNewsForArr['pubDate']     = $itemNews->pubDate;
+                $itemNewsForArr['linkChanel']  = $sxml->channel->link;
+                $this->_listNews[]             = $itemNewsForArr;
+            }
+        }
+    }
 } 
