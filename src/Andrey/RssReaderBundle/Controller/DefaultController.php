9d7cae2c49c39adf5a75011090e236962ab0ed2f
avnenkovskyi <avnenkovskyi@avnenkovskyi-H55M-S2.(none)>
2014-01-21 16:50:35 +0200

    create Files directory, put link.txt to Files

--- src/Andrey/RssReaderBundle/Controller/DefaultController.php
+++ src/Andrey/RssReaderBundle/Controller/DefaultController.php
@@ -8,6 +8,9 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $kernel = $container->getService('kernel');
+        $path = $kernel->locateResource('@AdmeDemoBundle/path/to/file/Foo.txt');
+
         return $this->render('AndreyRssReaderBundle:Default:index.html.twig');
     }
 }
