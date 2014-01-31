8fe50e5c409310ffddcb9c082d772d4c3ef6f084
avnenkovskyi <avnenkovskyi@avnenkovskyi-H55M-S2.(none)>
2014-01-31 10:16:59 +0200

    add monolg channel rss

--- src/Andrey/RssReaderBundle/Controller/DefaultController.php
+++ src/Andrey/RssReaderBundle/Controller/DefaultController.php
@@ -6,109 +6,81 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use \Exception;
 
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     const COUNT_NEWS_ON_PAGE = 15;
 
+    public function __construct()
+    {
+
+    }
+
     public function indexAction(Request $request)
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:index.html.twig',
-                array('showPagin' => false)
-            );
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
+        return $this->render(
+            'AndreyRssReaderBundle:Default:index.html.twig',
+            array('showPagin' => false)
+        );
     }
 
     public function allAction($page)
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:all.html.twig',
-                array('allNews' => $this->get('RssReaderModel.model')
-                        ->getAllNews(self::COUNT_NEWS_ON_PAGE, $page),
-                    'page' => $page,
-                    'urlForPagin' => 'http://symfony/rss_reader/all/',
-                    'showPagin' => true,
-                    'paginator' => $this->get('RssReaderService.service')
-                            ->getPaginator(self::COUNT_NEWS_ON_PAGE, $page)
-                ));
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
+        return $this->render(
+            'AndreyRssReaderBundle:Default:all.html.twig',
+            array('allNews'     => $this->get('RssReaderModel.model')
+                                        ->getAllNews(self::COUNT_NEWS_ON_PAGE, $page),
+                  'page'        => $page,
+                  'urlForPagin' => 'http://symfony/rss_reader/all/',
+                  'showPagin'   => true,
+                  'paginator'   => $this->get('RssReaderService.service')
+                                        ->getPaginator(self::COUNT_NEWS_ON_PAGE, $page)
+            )
+        );
     }
 
     public function sourceAction()
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:source.html.twig',
-                array('chanels' => $this->get('RssReaderModel.model')->getChanelsWithCountNews(),
-                    'showPagin' => false)
-            );
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
-
+        return $this->render(
+            'AndreyRssReaderBundle:Default:source.html.twig',
+            array('chanels'   => $this->get('RssReaderModel.model')->getChanelsWithCountNews(),
+                  'showPagin' => false)
+        );
     }
 
     public function sourcenewsAction($id, $page)
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:sourcenews.html.twig',
-                array('news' => $this->get('RssReaderModel.model')
-                        ->getNewsByChanel($id, self::COUNT_NEWS_ON_PAGE, $page),
-                    'page' => $page,
-                    'urlForPagin' => "http://symfony/rss_reader/sourcenews/id/$id/page/",
-                    'showPagin' => true,
-                    'paginator' => $this->get('RssReaderService.service')
-                            ->getPaginator(self::COUNT_NEWS_ON_PAGE, $page, $id)
-                ));
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
-
+        return $this->render(
+            'AndreyRssReaderBundle:Default:sourcenews.html.twig',
+            array('news'        => $this->get('RssReaderModel.model')
+                                        ->getNewsByChanel($id, self::COUNT_NEWS_ON_PAGE, $page),
+                  'page'        => $page,
+                  'urlForPagin' => "http://symfony/rss_reader/sourcenews/id/$id/page/",
+                  'showPagin'   => true,
+                  'paginator'   => $this->get('RssReaderService.service')
+                                        ->getPaginator(self::COUNT_NEWS_ON_PAGE, $page, $id)
+            )
+        );
     }
 
     public function newsAction($id)
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:news.html.twig',
-                array('news' => $this->get('RssReaderModel.model')
-                        ->getNewsById($id),
-                    'showPagin' => false
-                ));
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
+        return $this->render(
+            'AndreyRssReaderBundle:Default:news.html.twig',
+            array('news'      => $this->get('RssReaderModel.model')
+                                      ->getNewsById($id),
+                  'showPagin' => false
+            )
+        );
     }
 
     public function updateAction()
     {
-        try {
-            return $this->render(
-                'AndreyRssReaderBundle:Default:updateResponse.html.twig',
-                $this->get('RssReaderService.service')->updateMethod()
-            );
-        } catch (Exception $e) {
-            file_put_contents(
-                $this->get('kernel')->locateResource('@AndreyRssReaderBundle/Files/logs/error.txt'),
-                $e->getMessage());
-        }
+        return $this->render(
+            'AndreyRssReaderBundle:Default:updateResponse.html.twig',
+            $this->get('RssReaderService.service')->updateMethod()
+        );
     }
 }
 
--- src/Andrey/RssReaderBundle/Services/RssReaderService.php
+++ src/Andrey/RssReaderBundle/Services/RssReaderService.php
@@ -12,110 +12,143 @@ class RssReaderService {
     protected $_listChanels   = array();
     protected $_listNews      = array();
     protected $_chanelUrls    = array('http://tsn.ua/');
+    protected $_logger        = null;
+    protected $_error         = false;
 
-    public function __construct($kernel, $pagin, $model)
+    public function __construct($kernel, $pagin, $model, $logger)
     {
         $this->_kernelService = $kernel;
         $this->_paginService  = $pagin;
         $this->_modelService  = $model;
+        $this->_logger        = $logger;
     }
 
     public function updateMethod()
     {
         $this->_populateNewsAndChanel($this->_getContentRss($this->_getContentFile()));
-
         $response['chanels'] = $this->_modelService->insertChanels($this->_listChanels);
         $this->_chanelToNews();
         $response['news']    = $this->_modelService->insertNews($this->_listNews);
+
+        $this->_error ? : $this->_logger->info('update successfully completed');
 
         return $response;
     }
 
     protected function _getContentFile()
     {
+        $localResource = '@AndreyRssReaderBundle/Files/links.txt';
+
         try {
-            return array_filter(explode(
-                "\n",
-                str_replace(
-                    "\r",
-                    '',
-                    file_get_contents(
-                        $this->_kernelService->locateResource('@AndreyRssReaderBundle/Files/links.txt')
+            return array_filter(
+                explode(
+                    "\n",
+                    str_replace(
+                        "\r",
+                        '',
+                        file_get_contents(
+                            $this->_kernelService->locateResource($localResource)
+                        )
                     )
-                )
                 )
             );
         } catch (Exception $e) {
-            $this->errorMessage =  $e->getMessage();
+            $this->_error = true;
+            $this->_logger->err('Unable to load ' . $localResource . ' file');
         }
     }
 
     protected function _getContentRss($linksToRss)
     {
-        foreach ($linksToRss as $itemLinkRss) {
-            $contentRss[] = file_get_contents($itemLinkRss);
-        }
+        try {
+            foreach ($linksToRss as $itemLinkRss) {
+                $contentRss[] = file_get_contents($itemLinkRss);
+            }
 
-        return $contentRss;
+            return $contentRss;
+        } catch (Exception $e) {
+            $this->_error = true;
+            $this->_logger->err('Unable to load ' . $itemLinkRss . ' file');
+        }
     }
 
     protected function _populateNewsAndChanel($contentsRss)
     {
-        foreach ($contentsRss as $itemXMLFile) {
-            $sxml   = new SimpleXMLElement($itemXMLFile, LIBXML_NOCDATA);
-            $chanel = new Chanels();
-            $chanel->setTitle($sxml->channel->title)
-                   ->setLink($sxml->channel->link);
+        try {
+            foreach ($contentsRss as $itemXMLFile) {
+                $sxml   = new SimpleXMLElement($itemXMLFile, LIBXML_NOCDATA);
+                $chanel = new Chanels();
+                $chanel->setTitle($sxml->channel->title)
+                       ->setLink($sxml->channel->link);
 
-            $this->_listChanels[] = $chanel;
+                $this->_listChanels[] = $chanel;
 
-            foreach ($sxml->channel->item as $itemNews) {
-                $news = new News();
-                $news->setTitle($itemNews->title)
-                     ->setLink($itemNews->link)
-                     ->setDescription(htmlspecialchars(strip_tags($itemNews->description)))
-                     ->setHashCode(md5($itemNews->description))
-                     ->setPubDate(date('Y-m-d H:i:s', strtotime($itemNews->pubDate)))
-                     ->setChanelId($sxml->channel->link)
-                     ->setImage($this->_listenerChanels((string)$sxml->channel->link, $itemNews));
+                foreach ($sxml->channel->item as $itemNews) {
+                    $news = new News();
+                    $news->setTitle($itemNews->title)
+                         ->setLink($itemNews->link)
+                         ->setDescription(htmlspecialchars(strip_tags($itemNews->description)))
+                         ->setHashCode(md5($itemNews->description))
+                         ->setPubDate(date('Y-m-d H:i:s', strtotime($itemNews->pubDate)))
+                         ->setChanelId($sxml->channel->link)
+                         ->setImage($this->_listenerChanels((string)$sxml->channel->link, $itemNews));
 
-                $this->_listNews[]  = $news;
+                    $this->_listNews[]  = $news;
+                }
             }
+        } catch (Exception $e) {
+            $this->_error = true;
+            $this->_logger->err($e->getMessage());
         }
     }
 
     protected function _chanelToNews()
     {
-        foreach ($this->_modelService->getAllChanels() as $itemChanel) {
-            foreach ($this->_listNews as $key => $news) {
-                if ($news->getChanelId() == $itemChanel->getLink()) {
-                    $this->_listNews[$key]->setChanelId($itemChanel->getId());
+        try {
+            foreach ($this->_modelService->getAllChanels() as $itemChanel) {
+                foreach ($this->_listNews as $key => $news) {
+                    if ($news->getChanelId() == $itemChanel->getLink()) {
+                        $this->_listNews[$key]->setChanelId($itemChanel->getId());
+                    }
                 }
             }
+        } catch (Exception $e) {
+            $this->_error = true;
+            $this->_logger->err($e->getMessage());
         }
     }
 
     protected function _listenerChanels($url, $news)
     {
-        if (!in_array($url, $this->_chanelUrls)) {
-            return property_exists($news, 'enclosure') ? $news->enclosure['url'] : '';
-        }
+        try {
+            if (!in_array($url, $this->_chanelUrls)) {
+                return property_exists($news, 'enclosure') ? $news->enclosure['url'] : '';
+            }
 
-        switch($url) {
-            case 'http://tsn.ua/':
-                preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i',
-                    (string)$news->description, $matches);
-                return $matches[1][0];
-                break;
+            switch($url) {
+                case 'http://tsn.ua/':
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i',
+                        (string)$news->description, $matches);
+                    return $matches[1][0];
+                    break;
+            }
+        } catch (Exception $e) {
+            $this->_error = true;
+            $this->_logger->err($e->getMessage());
         }
     }
 
     public function getPaginator($countOnPage, $page, $sourceId = null)
     {
-        if ($sourceId) {
-            return $this->_paginService->paginator($page, $this->_modelService->getCountNews($sourceId), $countOnPage);
-        } else {
-            return $this->_paginService->paginator($page, $this->_modelService->getCountNews(), $countOnPage);
+        try {
+            if ($sourceId) {
+                return $this->_paginService->paginator($page, $this->_modelService->getCountNews($sourceId), $countOnPage);
+            } else {
+                return $this->_paginService->paginator($page, $this->_modelService->getCountNews(), $countOnPage);
+            }
+        } catch (Exception $e) {
+            $this->_error = true;
+            $this->_logger->err($e->getMessage());
         }
     }
 
